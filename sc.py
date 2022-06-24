6ba6963934ef893a797d189325051e5db37f6bee
ksl <linandrew48@gmail.com>
2022-06-24 15:16:46 +0800

    move everything to github

---
+++ sc.py
@@ -0,0 +1,27 @@
+#software control of PTZ 
+import serial
+
+ser = serial.Serial("/dev/ttyUSB0", 2400)
+
+while True: 
+ d = input('decision: left(l) \n  right(r) \n up(u) \n down(d): ')
+ try:
+    if d=='r':
+     while True:
+      print('pan right')
+      ser.write(bytearray.fromhex('FF 01 00 02 20 00 23'))
+    if d=='l':
+        while True:
+         print('pan left')
+         ser.write(bytearray.fromhex('FF 01 00 04 3F 00 44'))
+    if d=='u':
+        while True:
+         print('pan left')
+         ser.write(bytearray.fromhex('FF 01 00 08 00 3F 48'))
+    if d=='d':
+        while True:
+         print('pan left')
+         ser.write(bytearray.fromhex('FF 01 00 10 20 00 31'))
+ except KeyboardInterrupt:
+    continue 
+ 
