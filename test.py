210c063a1aa055bc01fbe98c4cef8d3b054745a7
ksl <linandrew48@gmail.com>
2022-07-06 11:35:38 +0800

    modify read function

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def print_position(a):
     q2 = toHex(a[5])
 
     q_3 = '{0:0>2s}{1:0>2s}'.format(q1, q2)
-    q_final = int(q_3, 16) // 99.99
+    q_final = int(q_3, 16) // 99.9
     return q_final
 
 
@@ -48,7 +48,7 @@ def read_position():
     time.sleep(1)
     tilt_read = ser.read(7)
 
-    return  pan_read,tilt_read
+    return pan_read, tilt_read
 
 
 def mod(num):  # this function is for converting angle values to real values used in pelco command
@@ -87,23 +87,33 @@ def tilt(var):
 while True:
 
     reading = read_position()
-    print('current position ...\t pan:{0} ||  tilt:{1}'.format(print_position(reading[0]), print_position(reading[1])))
+    print('current position ...\t pan:{0} ||  tilt:{1}'.format(
+        print_position(reading[0]), print_position(reading[1])))
 
     z = input("Type (p) for pan and (t) for tilt : ")
 
     if z == 'p':
         f = int(input('type pan degree 0 to 350: '))
 
-        tmp = mod(f)
-        var_0 = toHex(tmp)
-        pan(var_0)
+        if f != print_position(reading[0]):
+
+            tmp = mod(f)
+            var_0 = toHex(tmp)
+            pan(var_0)
+        else:
+            print("You enter current pan position ")
 
     elif z == 't':
         f = int(input('type pan degree 0 to 60: '))
 
-        tmp = mod(f)
-        var_0 = toHex(tmp)
-        tilt(var_0)
+        if f != print_position(reading[0]):
+
+            tmp = mod(f)
+            var_0 = toHex(tmp)
+            tilt(var_0)
+
+        else:
+            print("You enter current tilt position ")
 
     else:
         continue
